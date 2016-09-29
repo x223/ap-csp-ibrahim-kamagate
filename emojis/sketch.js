529e312defe063e314ad2c53f52a762479903073
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-09-29 14:28:55 -0400

    Add files via upload

---
+++ emojis/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(200,200);
+}
+
+function draw() {
+  fill('yellow')
+  ellipse(100,100,100,100);
+  fill('black')
+  ellipse(80,80,10,10);
+  ellipse(110,80,10,10);
+  line(60,110,130,110);
+}
