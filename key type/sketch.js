63d73c8e0af0d8cb16c3e7c7ae03496133f6dbdf
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2016-10-18 14:24:33 -0400

    Add files via upload

--- key type/sketch.js
+++ key type/sketch.js
@@ -1,27 +1,30 @@
 var strokeColor = 'red';
-var easer = 'white'
+var easer = 'white';
 
 function setup() {
   createCanvas(400,400);
 }
 
 function draw() {
-  if (mouseIsPressed); {
+  if (mouseIsPressed) {
     strokeWeight(20);
-  stroke(strokeColor);
-  line(pmouseX,pmouseY,mouseX,mouseY);
-}
+    stroke(strokeColor);
+    line(pmouseX,pmouseY,mouseX,mouseY);
+  }
 }
 
 function keyTyped() {
   if (key === 'b') {
-  strokeColor = '#CC0BE8';
+  strokeColor = '#0411FF';
   } else if (key === 'r') {
   strokeColor = '#FF1E0C';
 } else if (key === 'e') {
-  strokeColor = easer
+  strokeColor = 'white';
   weight = 20 
-    
+} else if (key === 'g') {
+ strokeColor = '#E86A0C';
+} else if (key === 'p') {
+ strokeColor = '#B00CE8';
 
 }
 }
